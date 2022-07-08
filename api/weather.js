3f5705af5565f7bd9b597b7b66c72e8e9105d740
amalshehu <amalshehu@gmail.com>
2022-07-09 00:38:55 +0530

    Make weather app

--- api/weather.js
+++ api/weather.js
@@ -1,12 +1,55 @@
 const express = require("express")
 const router = express.Router()
+const request = require("request")
+const PImage = require("pureimage")
+const fs = require("fs")
+const schedule = require("node-schedule")
+
+let havesWeather = false
+
+function fetchAndSaveWeather() {
+  request(
+    "https://openweathermap.org/data/2.5/weather?id=1277333&appid=439d4b804bc8187953eb36d2a8c26a02",
+    function (error, response, body) {
+      const data = JSON.parse(body)
+      const fnt = PImage.registerFont("./Menlo.ttf", "Menlo")
+      fnt.load(() => {
+        const img = PImage.make(450, 50)
+        const ctx = img.getContext("2d")
+        ctx.imageSmoothingEnabled = true
+        ctx.fillStyle = "#000"
+        ctx.font = "16pt"
+        ctx.fillText(`Currently, ${data.weather[0].description}.`, 5, 40)
+        ctx.fillText(
+          `Today, ${data.main.temp}°C, ${data.weather[0].main}, ${data.name}`,
+          5,
+          20
+        )
+
+        PImage.encodePNGToStream(img, fs.createWriteStream("image.png"))
+          .then(() => {
+            console.log("Weather snapshot saved.")
+            havesWeather = true
+          })
+          .catch((e) => {
+            console.log("There was an error writing file.")
+          })
+      })
+    }
+  )
+}
+
+if (!havesWeather) {
+  fetchAndSaveWeather()
+}
+
+schedule.scheduleJob("0 0 * * * *", function () {
+  fetchAndSaveWeather()
+})
 
 router.get("/", async (req, res) => {
   try {
-    res.json({
-      status: 200,
-      message: "fetch data has successfully",
-    })
+    res.sendFile("image.png", { root: "./" })
   } catch (error) {
     console.error(error)
     return res.status(500).send("Server error")
