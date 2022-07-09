5ce098d43cd5e765177f858156dde9581d54d979
amalshehu <amalshehu@gmail.com>
2022-07-09 17:05:46 +0530

    Test node canvas

--- api/weather.js
+++ api/weather.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const PImage = require("pureimage")
+const { registerFont, createCanvas } = require("canvas")
+
 const axios = require("axios")
 const path = require("path")
 const fs = require("fs")
+const file = path.join(process.cwd(), "/", "Menlo.ttf")
+
+registerFont(file, { family: "Menlo" })
 const options = {
   method: "GET",
   url: "https://openweathermap.org/data/2.5/onecall?lat=12.9762&lon=77.6033&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02",
@@ -15,7 +20,6 @@ router.get("/", async (req, res) => {
   try {
     promise.then((response) => {
       const { data } = response
-      const file = path.join(process.cwd(), "/", "Menlo.ttf")
       const fnt = PImage.registerFont(file, "Menlo")
       fnt.load(() => {
         makeWeather(data, res)
@@ -26,6 +30,28 @@ router.get("/", async (req, res) => {
   }
 })
 
+router.get("/image", async (req, res) => {
+  try {
+    promise.then((response) => {
+      const { data } = response
+      makeWeather2(data, res)
+    })
+  } catch (error) {
+    return res.status(500).send("Server error")
+  }
+})
+
+function makeWeather2(data, res) {
+  const canvas = createCanvas(500, 500)
+  const ctx = canvas.getContext("2d")
+  ctx.antialias = "subpixel"
+  ctx.font = '48px "Comic Sans"'
+  ctx.fillText("But it works!", 20, 100)
+  res.writeHead(200, {
+    "Content-Type": "image/png",
+  })
+  res.end(canvas.toBuffer("image/png"))
+}
 function makeWeather(data, res) {
   const img = PImage.make(450, 70)
   const ctx = img.getContext("2d")
