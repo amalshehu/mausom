cc75bb7a46eaea9241a941aae8da72b772aee648
amalshehu <amalshehu@gmail.com>
2022-07-09 16:46:15 +0530

    Update API

--- api/weather.js
+++ api/weather.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const PImage = require("pureimage")
 const axios = require("axios")
 const path = require("path")
+const fs = require("fs")
 const options = {
   method: "GET",
-  url: "https://openweathermap.org/data/2.5/weather?id=1277333&appid=439d4b804bc8187953eb36d2a8c26a02",
+  url: "https://openweathermap.org/data/2.5/onecall?lat=12.9762&lon=77.6033&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02",
 }
 
 const promise = axios(options)
@@ -26,19 +27,21 @@ router.get("/", async (req, res) => {
 })
 
 function makeWeather(data, res) {
-  const img = PImage.make(450, 50)
+  const img = PImage.make(450, 70)
   const ctx = img.getContext("2d")
-  const c = parseFloat(data.main.temp) < 26 ? "#8ac926" : "#ff595e"
+  const c = parseFloat(data.current.temp) < 24 ? "#269dc9" : "#f7ff59"
   ctx.fillStyle = c
   ctx.fillRect(0, 0, img.width, img.height)
   ctx.fillStyle = "#000"
   ctx.font = "16pt"
-  ctx.fillText(`Today, ${data.weather[0].description}.`, 5, 40)
   ctx.fillText(
-    `Currently, ${data.main.temp}°C, ${data.weather[0].main}, ${data.name}`,
+    `Feels like, ${data.current.feels_like}°C, ${data.current.weather[0].main}, Bangaluru`,
     5,
     20
   )
+  ctx.fillText(`Today, ${data.current.weather[0].description}.`, 5, 40)
+  ctx.fillText(`${Date(data.dt).toString().split("GMT")[0]}`, 5, 60)
+
   res.writeHead(200, {
     "Content-Type": "image/png",
   })
